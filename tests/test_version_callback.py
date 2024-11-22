d4974ceb8f950ea4172cc58d439b09e504fb74cf
Arina Kolodeznikova <arinak1017@gmail.com>
2024-11-22 12:37:04 -0500

    modified the test for version callback, added .pytest_cahce to .gitignore

--- tests/test_version_callback.py
+++ tests/test_version_callback.py
@@ -3,19 +3,23 @@ from typer import Exit
 from app.callbacks import version_callback
 
 
-# def test_version_callback(capfd):
-#     """
-#     Test version callback function with flag provided
-#     """
-#     # Check that function raises an Exit exception upon completion
-#     with pytest.raises(Exit):
-#         version_callback(provided=True)
+def test_version_callback(mocker, capfd):
+    """
+    Test version callback function with flag provided
+    """
+    # Mock read_toml() to return a mock version
+    mock_toml_data = {"tool": {"poetry": {"version": "1.2.3"}}}
+    mocker.patch("app.callbacks.read_toml", return_value=mock_toml_data)
 
-#     # Capture the output
-#     captured = capfd.readouterr()
+    # Check that function raises an Exit exception upon completion
+    with pytest.raises(Exit):
+        version_callback(provided=True)
 
-#     # Check if the version was printed correctly
-#     assert captured.out.strip() == __version__
+    # Capture the output
+    captured = capfd.readouterr()
+
+    # Check if the version was printed correctly
+    assert captured.out.strip() == "1.2.3"
 
 
 def test_version_callback_without_flag(capfd):
